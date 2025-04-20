ec96d5ceb3a575a8cea918bc0f4619caafc27997
Leonardo Gurgel <leo@gurgel.io>
2025-04-20 14:39:32 -0300

    basic config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' va.vercel-scripts.com 'unsafe-inline' ${process.env.NODE_ENV === "development" ? "'unsafe-eval'" : ""};
+  img-src 'self' data:;
+  style-src 'self' 'unsafe-inline';
+  frame-ancestors 'none';
+  object-src 'none';
+  upgrade-insecure-requests;
+`;
 
-export default nextConfig;
+export default {
+	output: "standalone",
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "Content-Security-Policy",
+						value: cspHeader.replace(/\n/g, ""),
+					},
+				],
+			},
+		];
+	},
+} satisfies NextConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,4 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+/** @type {import('postcss-load-config').Config} */
+export default {
+	plugins: ["@tailwindcss/postcss"],
 };
-
-export default config;
