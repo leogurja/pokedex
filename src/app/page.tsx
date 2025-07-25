43428336924799e58becb277a28b6380ed3fd576
Leonardo Gurgel <leo@gurgel.io>
2025-07-25 12:17:34 -0300

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,27 @@ import { PokemonList } from "@/components/templates/pokemon-list";
 import { db } from "@/lib/db";
 
 export default async function Home() {
-	const initialData = {
-		count: await db.pokemon_species.count(),
-		results: await db.pokemon_species.findMany({
-			include: {
-				pokemon_species_names: {
-					where: {
-						local_language_id: 9 // english
-					}
-				},
-			},
-			take: 20,
-		})
-};
+	// const initialData = {
+	// 	count: await db.pokemon_species.count(),
+	// 	results: await db.pokemon_species.findMany({
+	// 		include: {
+	// 			pokemon_species_names: {
+	// 				where: {
+	// 					local_language_id: 9, // english
+	// 				},
+	// 			},
+	// 		},
+	// 		take: 20,
+	// 	}),
+	// };
 
 	return (
-		<main className="container grid grid-cols-4 mx-auto">
-			<PokemonList page={0} perPage={20} initialData={initialData} />
-		</main>
+		<div className="container grid grid-cols-4 mx-auto">
+			<PokemonList
+				page={0}
+				perPage={20}
+				initialData={{ count: 0, results: [] }}
+			/>
+		</div>
 	);
 }
